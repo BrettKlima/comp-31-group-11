d737dc2be6431209b2e40c2dbeb1fcff2b6b7b26
BrettKlima <75006378+BrettKlima@users.noreply.github.com>
2021-11-30 12:01:38 -0500

    Revert "Creation of entity classes"

--- group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ClientRepo.java
+++ group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ClientRepo.java
@@ -1,11 +1,8 @@
 package groupassign.roadco.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import groupassign.roadco.model.entities.Client;
-
 @Repository
-public interface ClientRepo extends CrudRepository<Client,Long>{
+public class ClientRepo {
     
 }
