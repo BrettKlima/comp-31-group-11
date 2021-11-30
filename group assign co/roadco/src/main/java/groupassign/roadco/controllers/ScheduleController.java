e79675c7b060b3a417383c5f944e2964e1b6024f
JoshRichmond599 <75049630+JoshRichmond599@users.noreply.github.com>
2021-11-30 14:27:21 -0500

    schedule

--- group assign co/roadco/src/main/java/groupassign/roadco/controllers/ScheduleController.java
+++ group assign co/roadco/src/main/java/groupassign/roadco/controllers/ScheduleController.java
@@ -1,8 +1,27 @@
 package groupassign.roadco.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import groupassign.roadco.services.ScheduleService;
 
 @Controller
 public class ScheduleController {
     
+    ScheduleService scheduleService;
+
+    @Autowired
+    public ScheduleController(ScheduleService scheduleService) { //Constructor
+        super();
+        this.scheduleService = scheduleService;
+    }
+
+    @GetMapping("/schedule")
+    public String getAllSchedule(Model model){
+        //model.addAttribute("schedule",scheduleService.findAll());
+        return "schedule";
+    }
+
 }
--- group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ScheduleRepo.java
+++ group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ScheduleRepo.java
@@ -1,10 +1,16 @@
 package groupassign.roadco.model.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+//import groupassign.roadco.model.entities.Employee;
 import groupassign.roadco.model.entities.Schedule;
 
 @Repository
 public interface ScheduleRepo extends CrudRepository<Schedule,Integer>{
-    
+    List<Schedule> findAll();
+    //List<Schedule> findByScheduleID(Schedule project);
+    //List<Schedule> findByEmployee(Employee employee);
 }
