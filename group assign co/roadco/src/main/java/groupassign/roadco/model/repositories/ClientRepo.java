f0dc7eca218bc3cf59a424a86ab6afec1bb40eb0
Bellageth <75006378+BrettKlima@users.noreply.github.com>
2021-11-30 13:36:25 -0500

    Created index page

--- group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ClientRepo.java
+++ group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ClientRepo.java
@@ -1,5 +1,6 @@
 package groupassign.roadco.model.repositories;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import groupassign.roadco.model.entities.Client;
 
