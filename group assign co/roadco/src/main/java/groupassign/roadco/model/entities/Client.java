8ba48904fed7dc5019cc6a8b7995eacc94a05839
Bellageth <75006378+BrettKlima@users.noreply.github.com>
2021-11-30 07:17:05 -0500

    creation of entities

--- group assign co/roadco/src/main/java/groupassign/roadco/model/entities/Client.java
+++ group assign co/roadco/src/main/java/groupassign/roadco/model/entities/Client.java
@@ -1,10 +1,13 @@
 package groupassign.roadco.model.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -17,4 +20,28 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="clientID")
     private Integer clientId;
+
+    @Column(name="clientName")
+    private String client_name;
+
+    @Column(name="clientCompany")
+    private String client_company;
+
+    @Column(name="clientPhone")
+    private String client_phone;
+
+    @Column(name="clientAddress")
+    private String client_address;
+
+    @OneToMany(mappedBy = "client")
+    private List<Project> projects;
+
+    public Client(){
+        clientId = 0;
+        client_name="";
+        client_company="";
+        client_phone="";
+        client_address="";
+        projects = null;
+    }
 }
