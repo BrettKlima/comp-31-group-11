f84740605776464dae18de503198357e24b35558
Bellageth <75006378+BrettKlima@users.noreply.github.com>
2021-12-05 13:48:56 -0500

    Creation of client queries

--- group assign co/roadco/src/main/java/groupassign/roadco/controllers/ClientController.java
+++ group assign co/roadco/src/main/java/groupassign/roadco/controllers/ClientController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import groupassign.roadco.services.ClientService;
 
@@ -23,4 +25,7 @@ public class ClientController {
         model.addAttribute("client", clientService.findAll());
         return "client";
     }
+
+ //   @PostMapping("/addClient")
+ //   public String newClient(@ModelAttribute)
 }
--- group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ClientRepo.java
+++ group assign co/roadco/src/main/java/groupassign/roadco/model/repositories/ClientRepo.java
@@ -2,6 +2,8 @@ package groupassign.roadco.model.repositories;
 
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import groupassign.roadco.model.entities.Client;
@@ -9,5 +11,12 @@ import groupassign.roadco.model.entities.Client;
 @Repository
 public interface ClientRepo extends CrudRepository<Client,Long>{
 
+    //Queries to find a client
+    List<Client> findByClientCompany(String clientCompany);
+    List<Client> findByClientName(String clientName);
+    List<Client> findByClientCompanyAndClientAddress(String clientCompany, String clientAddress);
+    //Queries to update a client
+
+    //Add a client
  
 }
